196d13b899dd51d69a967cafa7c74a719395b2c3
nowhyes <ixnerpeaxe@gmail.com>
2025-06-08 03:21:55 +0900

    Update TunnelWin.js

--- TunnelWin.js
+++ TunnelWin.js
@@ -46,7 +46,7 @@ class TunnelWin {
   display() {
     // 화면 초기화
     noStroke();
-    fill(238,246,255); // 터널 배경색
+    fill(174, 197, 218); // 터널 배경색
     rect(width/2, height/2, 1280, 720);
     noFill();
     strokeWeight(3);
